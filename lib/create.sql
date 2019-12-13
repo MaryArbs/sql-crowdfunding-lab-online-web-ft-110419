9974509d4cedcb67a40f6b4f64dfe74eeedcdbb3
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-13 15:04:38 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,3 @@
 CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, category  text, funding_goal INTEGER, start_date INTEGER. end_date INTEGER);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
